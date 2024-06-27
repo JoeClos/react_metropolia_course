bc4bdeeca57118113cd3b5136c30f5a829721ba1
Josephine Closan <josephine.closan@yahoo.com>
2024-06-27 14:29:56 +0300

    use keyword search

--- github-API/src/App.jsx
+++ github-API/src/App.jsx
@@ -3,25 +3,50 @@ import "./App.css";
 
 function App() {
   const [respositories, setRepositories] = useState([]);
+  const [ keyword, setKeyword] = useState("");
+  
+  // fetch data using react keyword
 
-  useEffect(() => {
-    fetch("https://api.github.com/search/repositories?q=react")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((resData) => {
-        setRepositories(resData.items);
-        console.log(resData.items);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/search/repositories?q=react")
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((resData) => {
+  //       setRepositories(resData.items);
+  //       console.log(resData.items);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  // fetc data using keyword
+  const repoFetch = () => {
+    fetch(`https://api.github.com/search/repositories?q=${keyword}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((resData) => {
+      setRepositories(resData.items);
+      console.log(resData.items);
+    })
+    .catch((err) => console.log(err));
+  }
+
+  const inputChanged = (event) => {
+    setKeyword(event.target.value);
+  }
 
   return (
     <>
       <h1>Repositories</h1>
+      <input type="text" placeholder="Keyword search" value={keyword} onChange={inputChanged}/>
+      <button onClick={repoFetch}>Search</button>
       <table>
         <tbody>
           <tr>
@@ -32,7 +57,7 @@ function App() {
             <tr key={repo.id}>
               <td>{repo.full_name}</td>
               <td>
-                <a href={repo.url} target="_blank">
+                <a href={repo.html_url} target="_blank">
                   {repo.url}
                 </a>
               </td>
@@ -40,6 +65,8 @@ function App() {
           ))}
         </tbody>
       </table>
+      
+    
     </>
   );
 }
