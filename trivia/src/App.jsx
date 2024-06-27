1b81e244e58fec5314ba199812a5448234772495
Josephine Closan <josephine.closan@yahoo.com>
2024-06-27 12:08:43 +0300

    trivia excercise

--- trivia/src/App.jsx
+++ trivia/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import DOMPurify from "dompurify";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [question, setQuestion] = useState({});
+  const [showAnswer, setShowAnswer] = useState(false);
+  const sanitizedElement = DOMPurify.sanitize(question.question);
+
+  const fetchQuestion = () => {
+    fetch("https://opentdb.com/api.php?amount=1")
+      .then((response) => response.json())
+      .then((resData) => {
+        setQuestion(resData.results[0]);
+        console.log(resData.results[0]);
+        setShowAnswer(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const answer = () => {
+    console.log(question.correct_answer);
+    setShowAnswer(true); // clear the answer after 5 seconds
+  };
 
   return (
-    <>
+    <div style={{ display: "flex", gap: "5rem", flexDirection: 'column'}}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={fetchQuestion}>New question</button>
+        <p dangerouslySetInnerHTML={{ __html: sanitizedElement }}></p>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <button onClick={answer}>Show correct answer</button>
+        {showAnswer && <p>{question.correct_answer}</p>}{" "}
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
