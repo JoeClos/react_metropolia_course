81e08bebe59f85a8a746615d280cc5941b1d78de
Josephine Closan <josephine.closan@yahoo.com>
2024-06-27 09:24:58 +0300

    networking with React exemple

---
+++ nasa/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [explanation, setExplanation] = useState("");
+  const [url, setUrl] = useState("");
+  const [mediaType, setMediaType] = useState("");
+  const [nasaData, setNasaData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then((response) => {
+        if(!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setExplanation(data.explanation);
+        setUrl(data.url);
+        setMediaType(data.mediaType);
+        setNasaData(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (!url) {
+    return <>Loading...</>;
+  } else if (mediaType === "image") {
+    return (
+      <>
+        <p>Explanation: {explanation}</p>
+        <img alt="Nasa APOD" src={url} style={{ width: "60%" }} />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <p>Explanation: {explanation}</p>
+      <iframe
+        width="520"
+        height="415"
+        src={url}
+        title="Nasa APOD"
+      ></iframe>{" "}
+      <p>
+        <a
+          href={"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"}
+          target="Nasa APOD"
+        >
+          Link
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default App;
